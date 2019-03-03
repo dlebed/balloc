bac0be00d5b1577d9e9448a5ddee308973d649b9
Dmitry Lebed <lebed.dmitry@gmail.com>
2019-03-03 22:03:53 +0300

    Extended list manipulation functions

--- list.h
+++ list.h
@@ -2,11 +2,22 @@
 #define _LIST_H
 
 #include <stdbool.h>
+#include <stdint.h>
 
 struct list_entry {
 	struct list_entry *next;
 	struct list_entry *prev;
 };
+
+#define list_container_of(ptr, type, member) ((type *)((uintptr_t)(ptr) - offsetof(type, member)))
+
+#define list_entry(ptr, type, member) list_container_of(ptr, type, member)
+#define list_fist_entry(head, type, member) list_entry((head)->next, type, member)
+
+static inline bool list_is_empty(const struct list_entry *head)
+{
+	return head->next == head;
+}
 
 static inline void list_head_init(struct list_entry *head)
 {
@@ -33,9 +44,22 @@ static inline void list_del(struct list_entry *entry)
 	entry->prev->next = entry->next;
 }
 
-static inline bool list_is_empty(struct list_entry *head)
+static inline struct list_entry *list_pop_head(struct list_entry *head)
 {
-	return head->next == head;
+	struct list_entry *entry;
+
+	if (list_is_empty(head))
+		return NULL;
+
+	entry = head->next;
+	list_del(entry);
+
+	return entry;
+}
+
+static inline struct list_entry *list_pop(struct list_entry *head)
+{
+	return list_pop_head(head);
 }
 
 #endif /* _LIST_H */
